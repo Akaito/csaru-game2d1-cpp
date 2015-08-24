b9ea4631fcdf1ef5a071377eff2cca2d9332d1bb
Chris Barrett <chris@codesaru.com>
2015-08-23 20:13:13 -0700

    Scaled blitting and format covnersion.

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,18 @@ static const int  s_screenWidth       = 640;
 static const int  s_screenHeight      = 480;
 static const char s_testImageSource[] = "kenney/platformer_redux/spritesheet_ground.png";
 
-static SDL_Window * g_window     = nullptr;
-static SDL_Surface * g_windowSurface   = nullptr;
-static SDL_Surface * g_testImage = nullptr;
+static SDL_Window *  g_window        = nullptr;
+static SDL_Surface * g_windowSurface = nullptr;
+static SDL_Surface * g_testImage     = nullptr;
+
+static SDL_Rect s_testRects[] = {
+    {   0,   0, 128, 128 },
+    { 128,   0, 128, 128 },
+    { 256,   0, 128, 128 },
+    {   0, 128, 128, 128 },
+    { 128, 128, 128, 128 },
+};
+static unsigned g_testRectIndex = 0;
 
 
 bool init () {
@@ -44,11 +53,24 @@ bool init () {
 
 bool loadMedia () {
 
-    g_testImage = IMG_Load(s_testImageSource);
-    if (!g_testImage) {
+    SDL_Surface * tempSurface = nullptr;
+
+    tempSurface = IMG_Load(s_testImageSource);
+    if (!tempSurface) {
         printf("Failed to load image at {%s}.  %s\n", s_testImageSource, SDL_GetError());
         return false;
     }
+    //g_testImage = tempSurface;
+    //return true;
+
+    g_testImage = SDL_ConvertSurface(tempSurface, g_windowSurface->format, 0 /* flags */);
+    if (!g_testImage) {
+        printf("Failed to convert and optimize loaded image format from {%s}.  %s\n", s_testImageSource, SDL_GetError());
+        return false;
+    }
+
+    SDL_FreeSurface(tempSurface);
+    tempSurface = nullptr;
 
     return true;
 
@@ -85,13 +107,28 @@ int main (int argc, char ** argv) {
 
     while (!readyToQuit) {
         while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT)
+            if (e.type == SDL_QUIT) {
                 readyToQuit = true;
+            }
+            else if (e.type == SDL_KEYDOWN) {
+                switch (e.key.keysym.sym) {
+                    case SDLK_ESCAPE: readyToQuit = true; break;
+
+                    case SDLK_UP:    g_testRectIndex = 1; break;
+                    case SDLK_DOWN:  g_testRectIndex = 2; break;
+                    case SDLK_LEFT:  g_testRectIndex = 3; break;
+                    case SDLK_RIGHT: g_testRectIndex = 4; break;
+
+                    default:         g_testRectIndex = 0; break;
+                }
+            }
         }
 
         // draw
         SDL_FillRect(g_windowSurface, nullptr /* rect */, SDL_MapRGB(g_windowSurface->format, 0x00, 0x00, 0x3F));
-        SDL_BlitSurface(g_testImage, nullptr /* srcRect */, g_windowSurface, nullptr /* destRect */);
+        SDL_Rect windRect = {0, 0, s_screenWidth, s_screenHeight};
+        SDL_BlitScaled(g_testImage, &s_testRects[g_testRectIndex], g_windowSurface, &windRect);
+        SDL_BlitSurface(g_testImage, &s_testRects[g_testRectIndex], g_windowSurface, nullptr /* destRect */);
         SDL_UpdateWindowSurface(g_window);
     }
 
