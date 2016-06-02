7a204ed9216b00d078d57a90fd1188c7a538476d
Chris Barrett <chris@codesaru.com>
2016-06-02 09:39:50 -0700

    build-and-run.sh stops on build error.

--- build-and-run.sh
+++ build-and-run.sh
@@ -3,7 +3,9 @@
 # run from 'build' dir (mkdir build && cd build && cmake .. && ../run.sh)
 
 make && cp -r ../working-dir/* ~/.codesaru/csaru-game2d1-cpp/
-pushd ../working-dir
-csaru-game2d1-cpp
-popd
+if [ $? -eq 0 ]; then
+	pushd ../working-dir
+	csaru-game2d1-cpp
+	popd
+fi
 
