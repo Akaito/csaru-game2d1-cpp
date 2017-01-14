aa5fa632d083a9091292025ca7e551e6da634484
Chris Barrett <chris@codesaru.com>
2017-01-13 18:10:13 -0800

    Tweak build-and-run cwd expectation.

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,7 +2,7 @@
 
 # run from 'build' dir (mkdir build && cd build && cmake .. && ../run.sh)
 
-make && cp -r ../working-dir/* ~/.codesaru/csaru-game2d1-cpp/
+make && cp -r working-dir/* ~/.codesaru/csaru-game2d1-cpp/
 if [ $? -eq 0 ]; then
 	csaru-game2d1-cpp
 fi
